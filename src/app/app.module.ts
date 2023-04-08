aa0a7f655538f1b2029905b787fcd2f68148973f
Justtife <farinubolu@gmail.com>
2023-04-08 02:43:29 +0200

    First Project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusComponent } from './pages/status/status.component';
+import { CallsComponent } from './pages/calls/calls.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TabComponent } from './component/tab/tab.component';
+import { SearchbarComponent } from './component/searchbar/searchbar.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    CallsComponent,
+    HomeComponent,
+    TabComponent,
+    ChatComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
