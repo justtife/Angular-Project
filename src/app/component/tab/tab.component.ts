40a5db360b703938dadc4000c5f08966d5cd7627
Justtife <farinubolu@gmail.com>
2023-04-08 20:47:27 +0200

    Reusable Tab bar component

--- src/app/component/tab/tab.component.ts
+++ src/app/component/tab/tab.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
 })
 export class TabComponent {
-
+  @Input() image_src: any;
+  @Input() image_alt: any;
+  @Input() user: any;
+  @Input() message: any;
+  @Input() time: any;
+  @Input() unread: any;
 }
